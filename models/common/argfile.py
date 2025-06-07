3676ba30bd4adf01406b8394e750064f44028afa
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2025-06-06 22:44:24 -0700

    extra arg-parser support

--- models/common/argfile.py
+++ models/common/argfile.py
@@ -14,5 +14,9 @@ def get_args():
     parser.add_argument("--width", type = int, default= 64, help = "width of the image")
     parser.add_argument("--channels", type=int, default= 3, help= 'number of channels in image')
     parser.add_argument("-cA" ,"--celebA", action = "store_true", help = "if flag given model will train on celebA dataset if not mnist dataset" )
+    parser.add_argument("--print_interval", type= int, default= 1, help= "when training how often we want to see loss")
+    parser.add_argument("--n_critic", type= int, default= 5, help= "steps for training critic model")
+    parser.add_argument("--c", type= int, default= 0.01, help= "weight cliping range for wgan")
+    parser.add_argument("--lamda", type= int, default= 10, help= "co-efficient term in gradient penaly for wgan-gp")
 
     return parser.parse_args()
