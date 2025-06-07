d9f60177dd16295e222a15d588b3f816e054cb2d
Suriya Prakash <148183120+SURIAPRAKASH1@users.noreply.github.com>
2025-06-07 03:21:32 -0700

    Update GAN.py

--- models/gan/GAN.py
+++ models/gan/GAN.py
@@ -7,7 +7,7 @@ import sys
 import os 
 import time
 import importlib
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))) 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../.'))) 
 print(sys.path)
 from datasets.mnist import mnist_dataset
 pritn("mnist dataset", mnist_dataset)
